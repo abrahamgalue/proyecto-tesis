e9bb18d5e376badbe8a22bb90f291340b075de94
Abraham Galue <abrahamgalue512@gmail.com>
2025-04-11 19:07:10 -0400

    Added functionality to dynamically change the background and status bar color based on the device theme

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,35 @@
 import '../global.css'
-
 import { Slot } from 'expo-router'
 
 import { SupabaseProvider } from '@/context/supabase-provider'
 import { StatusBar } from 'expo-status-bar'
 
+import { useColorScheme } from '@/lib/useColorScheme'
+import { useEffect } from 'react'
+import { colors, changeBackgroundColor } from '@/constants/colors'
+
 export default function AppLayout() {
+	const { colorScheme } = useColorScheme()
+
+	useEffect(() => {
+		if (colorScheme === 'dark') {
+			changeBackgroundColor(colors.dark.background)
+		} else {
+			changeBackgroundColor(colors.light.background)
+		}
+	}, [colorScheme])
+
 	return (
 		<SupabaseProvider>
 			<Slot />
-			<StatusBar style='auto' />
+			<StatusBar
+				backgroundColor={
+					colorScheme === 'dark'
+						? colors.dark.background
+						: colors.light.background
+				}
+				style='auto'
+			/>
 		</SupabaseProvider>
 	)
 }
