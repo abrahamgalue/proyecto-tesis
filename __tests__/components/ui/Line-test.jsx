9f73fc50cc5cc48eb347c385533514de1a41fcc7
Abraham Galue <abrahamgalue512@gmail.com>
2025-05-23 22:49:25 -0400

    Test and refactor to Line component

---
+++ __tests__/components/ui/Line-test.jsx
@@ -0,0 +1,79 @@
+import { render, screen } from '@testing-library/react-native'
+import Line from '@/components/ui/Line'
+import { LINE_ERR_MSG } from '@/lib/utils'
+
+describe('<Line />', () => {
+	let consoleErrorSpy
+
+	beforeEach(() => {
+		consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
+
+		jest.clearAllMocks()
+	})
+
+	afterEach(() => {
+		jest.restoreAllMocks()
+	})
+
+	test('should render a line', () => {
+		render(<Line width={70} height={1} />)
+
+		expect(screen.root).toBeOnTheScreen()
+	})
+
+	test('should render with custom styles', () => {
+		render(<Line />)
+
+		expect(screen.root).toHaveStyle({ height: 1, width: 1 })
+	})
+
+	test('should apply custom className', () => {
+		render(<Line />)
+
+		expect(screen.root.props.className).toContain('bg-foreground m-auto')
+	})
+
+	test('should render line with percentage values', () => {
+		render(<Line height='60%' />)
+
+		expect(screen.root).toBeOnTheScreen()
+	})
+
+	test('throws an error if both width and height are greater than 1', () => {
+		expect(() => render(<Line width={10} height={5} />)).toThrow(LINE_ERR_MSG)
+
+		expect(consoleErrorSpy).toHaveBeenCalled()
+	})
+
+	test('throws an error if both width and height are percentages', () => {
+		expect(() => render(<Line width='50%' height='75%' />)).toThrow(
+			LINE_ERR_MSG
+		)
+
+		expect(consoleErrorSpy).toHaveBeenCalled()
+	})
+
+	test('does not throw an error if width is greater than 1 and height is 1', () => {
+		render(<Line width={10} height={1} />)
+
+		expect(() => screen.root).not.toThrow()
+	})
+
+	test('does not throw an error if height is greater than 1 and width is 1', () => {
+		render(<Line width={1} height={5} />)
+
+		expect(() => screen.root).not.toThrow()
+	})
+
+	test('does not throw an error if width is a percentage and height is 1', () => {
+		render(<Line width='50%' height={1} />)
+
+		expect(() => screen.root).not.toThrow()
+	})
+
+	test('does not throw an error if height is a percentage and width is 1', () => {
+		render(<Line width={1} height='75%' />)
+
+		expect(() => screen.root).not.toThrow()
+	})
+})
--- components/ui/Line.jsx
+++ components/ui/Line.jsx
@@ -1,17 +1,8 @@
 import { View } from 'react-native'
+import { isValidLine } from '@/lib/utils'
 
 const Line = ({ width = 1, height = 1 }) => {
-	const isPercentage = (value) =>
-		typeof value === 'string' && value.endsWith('%')
-
-	if (
-		(width > 1 || isPercentage(width)) &&
-		(height > 1 || isPercentage(height))
-	) {
-		throw new Error(
-			"Invalid dimensions: Only one of 'width' or 'height' can be greater than 1 or a percentage to render a line."
-		)
-	}
+	isValidLine({ width, height })
 
 	const styles = {
 		width,
