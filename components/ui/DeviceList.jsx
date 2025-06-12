7d3f9cc0f77f72c2df8c9109222b828f55d71d07
Abraham Galue <abrahamgalue512@gmail.com>
2025-06-12 13:42:24 -0400

    Removed unnecessary optimizations and optimized device list performance

--- components/ui/DeviceList.jsx
+++ components/ui/DeviceList.jsx
@@ -1,32 +1,53 @@
-import { memo, useMemo } from 'react'
-import { FlatList, View } from 'react-native'
+import { useCallback } from 'react'
+import { FlatList, useWindowDimensions, View } from 'react-native'
 import { useDevices } from '@/store/devicesStore'
 import { useFilter } from '@/store/filterStore'
 import Device from '@/components/ui/Device'
 
-const DevicesList = memo(function DevicesList() {
+function DevicesList() {
 	const devices = useDevices()
 	const filter = useFilter()
 
-	const filteredDevices = useMemo(() => {
-		return filter === ''
-			? devices
-			: devices.filter((item) => item.type === filter)
-	}, [filter, devices])
+	const { width } = useWindowDimensions()
+	/**
+	 * Renders a filtered two-column device list.
+	 * Explains: 96 = (8px margin + 16px padding) * 2 sides * 2 columns.
+	 * ITEM_HEIGHT = (window width - 96) / 2.
+	 */
+	const ITEM_HEIGHT = (width - 96) / 2
+
+	const filteredDevices =
+		filter === '' ? devices : devices.filter((item) => item.type === filter)
+
+	const renderItem = useCallback(
+		({ item, index }) => (
+			<Device
+				key={item.id}
+				item={item}
+				num={index + 1}
+				itemSize={ITEM_HEIGHT}
+			/>
+		),
+		[]
+	)
 
 	return (
 		<View className='flex-1 px-4'>
 			<FlatList
-				initialNumToRender={5}
 				data={filteredDevices}
-				renderItem={({ item, index }) => (
-					<Device key={item.id} num={index + 1} item={item} />
-				)}
+				renderItem={renderItem}
+				initialNumToRender={5}
 				keyExtractor={(item) => item.id}
+				maxToRenderPerBatch={5}
 				numColumns={2}
+				getItemLayout={(_, index) => ({
+					length: ITEM_HEIGHT,
+					offset: ITEM_HEIGHT * index,
+					index
+				})}
 			/>
 		</View>
 	)
-})
+}
 
 export default DevicesList
