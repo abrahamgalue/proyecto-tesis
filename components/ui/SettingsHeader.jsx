045e4d6386cf00ccf70432d7fa968e1bfaa28648
Abraham Galue <abrahamgalue512@gmail.com>
2025-06-22 18:18:28 -0400

    Add static user image

--- components/ui/SettingsHeader.jsx
+++ components/ui/SettingsHeader.jsx
@@ -1,15 +1,12 @@
 import { memo, useEffect } from 'react'
 import { View } from 'react-native'
-import { useColorScheme } from '@/lib/useColorScheme'
 import { useSupabase } from '@/context/supabase-provider'
 import { useUsername, useUserActions } from '@/store/userStore'
-import { AccountCircleIcon } from '@/components/ui/Icons/Icons'
-import { colors } from '@/constants/colors'
+import { Image } from '@/components/image'
 import { Text } from '@/components/text'
 import { GenericSkeleton } from '@/components/ui/skeletons'
 
 function SettingsHeader() {
-	const { isDarkColorScheme } = useColorScheme()
 	const { session } = useSupabase()
 	const username = useUsername()
 	const { getUsername } = useUserActions()
@@ -22,12 +19,14 @@ function SettingsHeader() {
 
 	return (
 		<View className='h-24 w-full flex-row items-center gap-2 rounded-t-3xl bg-to-gradient px-6'>
-			<AccountCircleIcon
-				width={33}
-				height={33}
-				color={
-					isDarkColorScheme ? colors.dark.foreground : colors.light.foreground
-				}
+			<Image
+				style={{
+					contentFit: 'contain',
+					width: 33,
+					height: 33
+				}}
+				className='rounded-full'
+				source={require('@/assets/account.jpg')}
 			/>
 			{!!username ? (
 				<Text className='text-white'>{username}</Text>
